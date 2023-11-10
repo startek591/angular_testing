03960eb7491a0b49d0edadb0f66619152e5d7469
Jonathan Fambro <startek591@gmail.com>
2023-11-10 16:07:39 -0500

    Fixed issue, component restarts value after each spec -- refactor-fix

--- testing-demo/src/app/03-setup-and-teardown/vote.component.spec.ts
+++ testing-demo/src/app/03-setup-and-teardown/vote.component.spec.ts
@@ -1,7 +1,12 @@
 import { VoteComponent } from './vote.component';
 
 describe('VoteComponent', () => {
-  let component = new VoteComponent();
+  let component: VoteComponent;
+
+  beforeEach(() => {
+    component = new VoteComponent();
+  });
+
   it('should increment totalVotes when upvoted', () => {
     component.upVote();
 
