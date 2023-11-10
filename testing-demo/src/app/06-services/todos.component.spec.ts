bdcb3333b3a005015565fc2eda6769e14a968e0e
Jonathan Fambro <startek591@gmail.com>
2023-11-10 17:25:17 -0500

    Example of Working with Spies however, not working because of older version

---
+++ testing-demo/src/app/06-services/todos.component.spec.ts
@@ -0,0 +1,24 @@
+// import { TodosComponent } from './todos.component';
+// import { TodoService } from './todo.service';
+// import { Observable } from 'rxjs/Observable';
+// import 'rxjs/add/observable/from';
+
+// describe('TodosComponent', () => {
+//   let component: TodosComponent;
+//   let service: TodoService;
+//   beforeEach(() => {
+//     service = new TodoService(null);
+//     component = new TodosComponent(service);
+//   });
+
+//   it('should set todos property with the items returned from the server', () => {
+//     let todos = [1, 2, 3];
+//     spyOn(service, 'geTodos').and.callFake(() => {
+//       return Observable.from([[ todos ]]);
+//     });
+
+//     component.ngOnInt();
+
+//     expect(component.todos.length).toBe(todos);
+//   });
+// });
