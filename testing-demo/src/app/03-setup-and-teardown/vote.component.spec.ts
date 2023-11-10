e5f204e8b603f472044f003cc81ccd5b5c472558
Jonathan Fambro <startek591@gmail.com>
2023-11-10 16:03:38 -0500

    Issue because this test is fagile and the first test is taking the value and inputting into the second test

--- testing-demo/src/app/03-setup-and-teardown/vote.component.spec.ts
+++ testing-demo/src/app/03-setup-and-teardown/vote.component.spec.ts
@@ -1,21 +1,16 @@
 import { VoteComponent } from './vote.component';
 
 describe('VoteComponent', () => {
+  let component = new VoteComponent();
   it('should increment totalVotes when upvoted', () => {
-    // Arrange
-    let component = new VoteComponent();
-    // Act
     component.upVote();
-    // Assert
+
     expect(component.totalVotes).toBe(1);
   });
 
   it('should decrement totalVotes when downvotes', () => {
-    // Arrange
-    let component = new VoteComponent();
-    // Act
     component.downVote();
-    // Assert
+
     expect(component.totalVotes).toBe(-1);
   });
 });
