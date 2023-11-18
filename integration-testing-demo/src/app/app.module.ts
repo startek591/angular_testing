f516b46162c07d91cbf3c8e9702fa52a346e969e
Jonathan Fambro <startek591@gmail.com>
2023-11-18 01:30:37 -0500

    Add routes, voter components, and integration

--- integration-testing-demo/src/app/app.module.ts
+++ integration-testing-demo/src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-
+import { VoterComponent } from './voter/voter.component';
+import { routes } from './app.routes';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, VoterComponent],
+  imports: [RouterModule.forRoot(routes), BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
