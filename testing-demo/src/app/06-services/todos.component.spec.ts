aca6b15b5aea0f2148436c3ebe2a20282c315528
Jonathan Fambro <startek591@gmail.com>
2023-11-17 21:36:40 -0500

    Interaction Testing

--- testing-demo/src/app/06-services/todos.component.spec.ts
+++ testing-demo/src/app/06-services/todos.component.spec.ts
@@ -1,6 +1,6 @@
 import { TodosComponent } from './todos.component';
 import { TodeService } from './todo.services';
-import { from } from 'rxjs';
+import { from, empty, throwError } from 'rxjs';
 
 describe('TodosComponent', () => {
   let component: TodosComponent;
@@ -20,5 +20,33 @@ describe('TodosComponent', () => {
     component.ngOnInit();
 
     expect(component.todos.length).toBe(3);
+  });
+
+  it('should call the server to save the changes whne a new todo item is added', () => {
+    let spy = spyOn(service, 'add').and.callFake((t) => {
+      return empty();
+    });
+
+    component.add();
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should add the new todo return from the server', () => {
+    let todo = { id: 1 };
+    spyOn(service, 'add').and.returnValue(from([todo]));
+
+    component.add();
+
+    expect(component.todos.indexOf(todo)).toBeGreaterThan(-1);
+  });
+
+  it('should set the message property if server returns an error when adding a new todo', () => {
+    let error = 'error from the server';
+    spyOn(service, 'add').and.returnValue(throwError(error));
+
+    component.add();
+
+    expect(component.message).toBe(error);
   });
 });
