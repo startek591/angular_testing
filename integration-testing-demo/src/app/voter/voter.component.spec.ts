8dd65fee30e651659a14e2725280664d5d564e91
Jonathan Fambro <startek591@gmail.com>
2023-11-18 02:00:03 -0500

    Testing Property, Class Bindings and Event Bindings

--- integration-testing-demo/src/app/voter/voter.component.spec.ts
+++ integration-testing-demo/src/app/voter/voter.component.spec.ts
@@ -1,4 +1,6 @@
 import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
 import { VoterComponent } from './voter.component';
 
 describe('VoterComponent', () => {
@@ -13,5 +15,31 @@ describe('VoterComponent', () => {
     component = fixture.componentInstance;
   });
 
-  it('', () => {});
+  it('should render total votes', () => {
+    component.othersVote = 20;
+    component.myVote = 1;
+
+    fixture.detectChanges();
+
+    let de = fixture.debugElement.query(By.css('.vote-count'));
+    let el: HTMLElement = de.nativeElement;
+
+    expect(el.innerText).toContain('21');
+  });
+
+  it('should highlight the upvote button if I have upvoted', () => {
+    component.myVote = 1;
+    fixture.detectChanges();
+
+    let de = fixture.debugElement.query(By.css('.glyphicon-menu-up'));
+
+    expect(de.classes['highlighted']).toBeTruthy();
+  });
+
+  it('should increase total votes when I click the upvote button', () => {
+    let button = fixture.debugElement.query(By.css('.glyphicon-menu-up'));
+    button.triggerEventHandler('click', null);
+
+    expect(component.totalVotes).toBe(1);
+  });
 });
