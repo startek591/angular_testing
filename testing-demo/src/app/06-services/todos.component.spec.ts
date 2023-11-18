b201305dc5583a2f03e12ccff41ae6c3784072a2
Jonathan Fambro <startek591@gmail.com>
2023-11-18 00:57:21 -0500

    Interaction Testing

--- testing-demo/src/app/06-services/todos.component.spec.ts
+++ testing-demo/src/app/06-services/todos.component.spec.ts
@@ -49,4 +49,22 @@ describe('TodosComponent', () => {
 
     expect(component.message).toBe(error);
   });
+
+  it('should call the server to delete a todo item if the user confirms', () => {
+    spyOn(window, 'confirm').and.returnValue(true);
+    let spy = spyOn(service, 'delete').and.returnValue(empty());
+
+    component.delete(1);
+
+    expect(spy).toHaveBeenCalledWith(1);
+  });
+
+  it('should Not call the server to delete a todo item if the user cancels', () => {
+    spyOn(window, 'confirm').and.returnValue(false);
+    let spy = spyOn(service, 'delete').and.returnValue(empty());
+
+    component.delete(1);
+
+    expect(spy).not.toHaveBeenCalled();
+  });
 });
