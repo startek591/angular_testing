9542d9994fbbc3910d78698cd8be0f576c4f6382
Jonathan Fambro <startek591@gmail.com>
2023-11-18 02:21:37 -0500

    Provide the dependencies for todo services

---
+++ integration-testing-demo/src/app/todos/todos.component.spec.ts
@@ -0,0 +1,27 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TodoService } from './todo.service';
+import { TodosComponent } from './todos.component';
+import { HttpClientModule } from '@angular/common/http';
+
+describe('TodosComponent', () => {
+  let component: TodosComponent;
+  let fixture: ComponentFixture<TodosComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      declarations: [TodosComponent],
+      providers: [TodoService],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TodosComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
